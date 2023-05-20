ce934798652089e2746d204e980c21882f179a70
Luis Sánchez <wichis6661@gmail.com>
2023-05-20 07:01:59 -0600

    front-registro added

---
+++ gamesApp/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+
+
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
+@NgModule({
+
+  imports: [ RouterModule.forRoot( routes ) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
